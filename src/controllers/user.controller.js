11df6a26604c8dc9b52c8b7047f13fcf67aa8700
amankumar213564-coder <amankumar213564@gmail.com>
2026-03-19 11:28:55 +0000

    eror

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,35 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { registerUserDB, updateUserDB } from "../services/user.service.js";
+import {
+    registerUserDB,
+    updateUserDB,
+    getUserDB,
+    deleteUserDB,
+} from "../services/user.service.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     await registerUserDB(req.body);
     return ApiResponse(res, 200, "user created successfully");
 });
 
+const getUser = asyncHandler(async (req, res) => {
+    const data = await getUserDB(req.user._id);
+
+    return ApiResponse(res, 200, "User fetched", data);
+});
+
 const updateUser = asyncHandler(async (req, res) => {
     const data = await updateUserDB(req.user._id, req.body);
-    if (!updatedUser) throw new ApiError(500, "Unable to update user data");
+    if (!data) throw new ApiError(500, "Unable to update user data");
 
     return ApiResponse(res, 200, "User updated successfully", data);
 });
 
-export { registerUser, updateUser };
+const deleteUser = asyncHandler(async (req, res) => {
+    const data = await deleteUserDB(req.user._id);
+
+    return ApiResponse(res, 200, "Successfully removed user from DB ");
+});
+
+export { registerUser, getUser, updateUser, deleteUser };
--- src/validation/payment.validator.js
+++ src/validation/payment.validator.js
@@ -1,9 +1,12 @@
 import { z } from "zod";
 import { objectId } from "./order.validator.js";
+
 const paymentValidator = z
     .object({
         userId: objectId,
+
         orderId: objectId,
+
         status: z
             .enum([
                 "pending",
@@ -13,18 +16,25 @@ const paymentValidator = z
                 "cancelled",
                 "refunded",
             ])
+            .optional()
             .default("pending"),
-        amount: z.number().positive("Amount must be positive"),
+
+        amount: z.coerce.number().positive("Amount must be positive"),
+
         paymentMethod: z.enum(["card", "upi", "netbanking"], {
             required_error: "Payment method is required",
         }),
+
         gateway: z.object({
-            name: z.string(),
-            transactionId: objectId,
+            name: z.string().min(1, "Gateway name is required"),
+            transactionId: z.string().min(1, "Transaction ID is required"),
         }),
+
         failureReason: z.string().optional(),
-        current: z.string().default("INR").optional(),
+
+        currency: z.string().optional().default("INR"),
     })
+    .strict()
     .refine(
         (data) => {
             if (data.status === "failed") {
