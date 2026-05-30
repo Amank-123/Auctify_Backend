a8823c39224cefbfa1ad277b78e7bbe3dc367f37
Amank-123 <akaman0055@gmail.com>
2026-05-31 01:09:33 +0530

    checking sendotp service

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -4,11 +4,19 @@ const sendEmail = async (email, otp) => {
     console.log("A");
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        tls: {
+            rejectUnauthorized: false,
+        },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
     });
 
     console.log("B");
