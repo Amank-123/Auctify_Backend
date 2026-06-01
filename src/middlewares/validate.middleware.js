81994bfebdd1c10222cb7bf34c024450e8b2a64e
Amank-123 <akaman0055@gmail.com>
2026-06-02 03:35:28 +0530

    just checking through log

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,3 +1,6 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
 export const validateData = (schema) =>
     asyncHandler((req, res, next) => {
         console.log("REQ BODY:", req.body);
